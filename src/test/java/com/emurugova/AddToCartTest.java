9d39a23c8289494f86f65e9bac1c6a56211ecd08
Елена <murugka31@gmail.com>
2023-02-16 09:20:14 +0300

    add main code

---
+++ src/test/java/com/emurugova/AddToCartTest.java
@@ -0,0 +1,58 @@
+package com.emurugova;
+
+import com.emurugova.config.CredentialsConfig;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Cookie;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static io.restassured.RestAssured.given;
+
+public class AddToCartTest extends TestBase{
+
+public static CredentialsConfig credentials = ConfigFactory.create(CredentialsConfig.class);
+
+    @Test
+    void addToCartTest() {
+        String authorizationCookie =
+                given()
+                        .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                        .formParam("Email", credentials.login())
+                        .formParam("Password", credentials.password())
+                        .when()
+                        .post("https://demowebshop.tricentis.com/login")
+                        .then()
+                        .statusCode(302)
+                        .extract()
+                        .cookie("NOPCOMMERCE.AUTH");
+
+        open("/Themes/DefaultClean/Content/images/logo.png");
+        getWebDriver().manage().addCookie(
+                new Cookie("NOPCOMMERCE.AUTH", authorizationCookie));
+
+        String response =
+                given()
+                        .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                        .body("product_attribute_72_5_18=53&product_attribute_72_6_19=54&" +
+                              "product_attribute_72_3_20=57&addtocart_72.EnteredQuantity=1")
+                        .cookie("NOPCOMMERCE.AUTH", authorizationCookie)
+                        .when()
+                        .post("https://demowebshop.tricentis.com/addproducttocart/details/72/1")
+                        .then()
+                        .extract()
+                        .path("updatetopcartsectionhtml");
+
+        open("/");
+        $(".account").shouldHave(text(credentials.login()));
+        $(".ico-cart").shouldHave(text(response));
+
+        System.out.println(response);
+    }
+}
+
+
+
+
---
+++ src/test/java/com/emurugova/TestBase.java
@@ -0,0 +1,12 @@
+package com.emurugova;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestBase {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demowebshop.tricentis.com";
+    }
+}
